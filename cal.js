1f3aba989f8289c7d8cba939ccf606209305e945
Snipkekt <146439025+Snipkekt@users.noreply.github.com>
2026-01-03 22:19:48 +0000

    fixed NaN

--- cal.js
+++ cal.js
@@ -1,31 +1,30 @@
 
-const inputBox=document.querySelector(".val");
-const resultBox=document.querySelector(".result");
-const buttons=document.querySelectorAll(".btn button");
-buttons.forEach(button=>{
-    button.addEventListener("click",function(){
-        const val=this.innerText;
-        if(val==="C"){
-            inputBox.value="";
-            resultBox.value="0";
-        }else if(val==="="){
-            try{
-                const res=eval(inputBox.value);
-                if(res===Infinity || res===-Infinity){
-                    resultBox.value="Error";
+const inputBox = document.querySelector(".val");
+const resultBox = document.querySelector(".result");
+const buttons = document.querySelectorAll(".btn button");
+buttons.forEach(button => {
+    button.addEventListener("click", function () {
+        const val = this.innerText;
+        if (val === "C") {
+            inputBox.value = "";
+            resultBox.value = "0";
+        } else if (val === "=") {
+            try {
+                const res = eval(inputBox.value);
+                if (res === Infinity || res === -Infinity || isNaN(res)) {
+                    resultBox.value = "Error";
                     return;
                 }
-                resultBox.value =res;
+                resultBox.value = res;
             }
-            catch(e){
-                resultBox.value="Error";
+            catch (e) {
+                resultBox.value = "Error";
             }
-           
+
         }
 
-        else{
-            inputBox.value+=val;
+        else {
+            inputBox.value += val;
         }
     });
 });
-        
