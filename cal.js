b761eb21119bd7d79896e0890ee62260e8095810
Snipkekt <146439025+Snipkekt@users.noreply.github.com>
2026-01-06 18:39:21 +0300

    ok back ig

--- cal.js
+++ cal.js
@@ -1,56 +1,3 @@
-
-const evaluate = (expr) => {
-    expr = expr.replace(/\s/g, '');
-    const tokens = [];
-    let i = 0;
-    while (i < expr.length) {
-        if (/\d|\./.test(expr[i])) {
-            let num = '';
-            while (i < expr.length && /\d|\./.test(expr[i])) {
-                num += expr[i];
-                i++;
-            }
-            tokens.push(parseFloat(num));
-        } else if ('+-*/'.includes(expr[i])) {
-            tokens.push(expr[i]);
-            i++;
-        } else {
-            throw new Error('Invalid character');
-        }
-    }
-    const stack = [];
-    let j = 0;
-    while (j < tokens.length) {
-        if (tokens[j] === '*' || tokens[j] === '/') {
-            const op = tokens[j];
-            const left = stack.pop();
-            j++;
-            const right = tokens[j];
-            if (op === '*') {
-                stack.push(left * right);
-            } else {
-                if (right === 0) throw new Error('Division by zero');
-                stack.push(left / right);
-            }
-        } else {
-            stack.push(tokens[j]);
-        }
-        j++;
-    }
-    if (stack.length === 0) throw new Error('Empty expression');
-    let result = stack[0];
-    for (let k = 1; k < stack.length; k += 2) {
-        const op = stack[k];
-        const right = stack[k + 1];
-        if (op === '+') {
-            result += right;
-        } else {
-            result -= right;
-        }
-    }
-    return result;
-};
-
 const inputBox = document.querySelector(".val");
 const resultBox = document.querySelector(".result");
 const buttons = document.querySelectorAll(".btn button");
@@ -62,7 +9,7 @@ buttons.forEach(button => {
             resultBox.value = "0";
         } else if (val === "=") {
             try {
-                const res = evaluate(inputBox.value);
+                const res = eval(inputBox.value);
                 if (res === Infinity || res === -Infinity || isNaN(res)) {
                     resultBox.value = "Error";
                     return;
